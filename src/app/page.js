ebbe037b95d041004ac85bd1d9819178b9b48aa2
Alberte <alan0002@stud.kea.dk>
2024-11-12 13:52:32 +0100

    card og products

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Buybnt from "@/components/Buybnt";
 import Card from "@/components/Card";
 import Menu from "@/components/Menu";
+import Singleview from "@/components/Singleview";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="bg-background">
-      <Menu/>
+      <Menu />
       <Card />
     </div>
   );
