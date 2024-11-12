ef95f61133f64c2e05b158760ba20cade33c01d6
Alberte <alan0002@stud.kea.dk>
2024-11-12 12:30:53 +0100

    lidt knap og card

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Buybnt from "@/components/Buybnt";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Hej cat</h1>
+      <Buybnt />
     </div>
   );
 }
---
+++ src/components/Buybnt.jsx
@@ -0,0 +1,11 @@
+const Buybnt = () => {
+  return (
+    <div>
+      <button className="border-2 rounded-15 border-blue-500 pr-5 pl-5">
+        Buy Here
+      </button>
+    </div>
+  );
+};
+
+export default Buybnt;
