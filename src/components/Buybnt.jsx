b28e7b8a6dc7ddb6ac6fc15d3d077fac2063df2c
Alberte <alan0002@stud.kea.dk>
2024-11-12 19:44:13 +0100

    single view og product

--- src/components/Buybnt.jsx
+++ src/components/Buybnt.jsx
@@ -1,7 +1,7 @@
 const Buybnt = () => {
   return (
     <div>
-      <button className=" rounded-15  bg-background text-lightblue pr-5 pl-5 hover:bg-lightgreen hover:text-black">
+      <button className=" rounded-15  bg-background text-lightblue pr-7 pl-7 p-2 hover:bg-lightgreen hover:text-black mt-5">
         Læg I Kurv
       </button>
     </div>
