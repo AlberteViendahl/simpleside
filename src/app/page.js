ac8419c2a32dab7fdc8330bfcfc07812855e3c9e
Catharina <catharina2002@gmail.com>
2024-11-13 12:58:23 +0100

    opdateret page for at undggå gentagelser

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
 import Buybnt from "@/components/Buybnt";
 import Card from "@/components/Card";
+import Dropdown from "@/components/Dropdown";
 import Menu from "@/components/Menu";
+import Review from "@/components/Review";
+import Productside from "@/components/Productside";
 import Singleview from "@/components/Singleview";
 import Image from "next/image";
 
@@ -8,7 +11,9 @@ export default function Home() {
   return (
     <div className="bg-background">
       <Menu />
-      <Card />
+      <Productside/>
+      <Review/>
+
     </div>
   );
 }
