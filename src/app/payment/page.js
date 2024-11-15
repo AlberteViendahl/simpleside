890f50b3a1c2ed61cfa392a4121cbefaa5e6da41
Catharina <catharina2002@gmail.com>
2024-11-15 20:18:16 +0100

    qr mp

--- src/app/payment/page.js
+++ src/app/payment/page.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const Betaling = async ({ searchParams }) => {
   const cart = searchParams.cart; // Modtag cart-data fra URL
 
@@ -16,7 +18,15 @@ const Betaling = async ({ searchParams }) => {
   const totalPrice = parsedCart.reduce((acc, item) => acc + item.price, 0);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-lightblue text-background">
+    <div className="flex flex-col-2 gap-10 items-center justify-center h-screen bg-lightblue text-background">
+      {/* Korrekt opsætning af Next.js Image */}
+      <div>
+      <Image 
+        src="/img/catogA.jpg" // Sti relativt til public-mappen
+        width={250} 
+        height={350} 
+        alt="QR kode" 
+      /></div><div>
       <h1 className="text-3xl font-bold mb-4">Betalingsside</h1>
       <p className="text-lg mb-4">Her vil du kunne gennemføre din betaling.</p>
       <ul className="mb-4">
@@ -28,6 +38,7 @@ const Betaling = async ({ searchParams }) => {
         ))}
       </ul>
       <h2 className="text-xl font-bold">Total: {totalPrice} kr</h2>
+    </div>
     </div>
   );
 };
